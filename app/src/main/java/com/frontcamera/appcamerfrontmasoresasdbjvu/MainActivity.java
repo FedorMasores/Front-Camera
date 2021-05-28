d88ac6d6bd4d65b42914bf48ffd16e77d0593a4e
Fedor <fyodor.masor.smh@gmail.com>
2021-05-28 12:33:37 +0300

    fixed camera launch

--- app/src/main/java/com/frontcamera/appcamerfrontmasoresasdbjvu/MainActivity.java
+++ app/src/main/java/com/frontcamera/appcamerfrontmasoresasdbjvu/MainActivity.java
@@ -1,5 +1,7 @@
 package com.frontcamera.appcamerfrontmasoresasdbjvu;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -8,31 +10,81 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView btnSwipe;
+    private int i = 0;
     private Camera mCam;
     private MirrorView mCamPreview;
-    private int mCameraId = 0;
+    private int mCameraId = Camera.CameraInfo.CAMERA_FACING_FRONT;
     private FrameLayout mPreviewLayout;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 50) {
+            for (int i = 0; i < permissions.length; i++) {
+                String permission = permissions[i];
+                int grantResult = grantResults[i];
+                if (permission.equals(Manifest.permission.CAMERA)) {
+                    if (grantResult == PackageManager.PERMISSION_GRANTED) {
+                        startCameraInLayout(mPreviewLayout, mCameraId);
+                    } else {
+                        requestPermissions(new String[]{Manifest.permission.CAMERA}, 50);
+                    }
+                }
+            }
+        }
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSwipe = findViewById(R.id.btn_swipe);
+
         mCameraId = findFirstFrontFacingCamera();
         mPreviewLayout = (FrameLayout) findViewById(R.id.camPreview);
         mPreviewLayout.removeAllViews();
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 50);
-        }
-        startCameraInLayout(mPreviewLayout, mCameraId);
+
+        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, 50);
+        onRequestPermissionsResult(50, new String[] {Manifest.permission.CAMERA}, new int[]{50});
+
+//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+//            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 50);
+//        }
+//
+//        startCameraInLayout(mPreviewLayout, mCameraId);
+
+        btnSwipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (i != 0) {
+                    startCameraInLayout(mPreviewLayout, mCameraId);
+                    i = 0;
+                } else {
+                    startCameraInLayout(mPreviewLayout, Camera.CameraInfo.CAMERA_FACING_BACK);
+                    i = 1;
+                }
+            }
+        });
     }
+
+
 
 
     private int findFirstFrontFacingCamera() {
@@ -57,7 +109,7 @@ public class MainActivity extends AppCompatActivity {
             mCam = null;
         }
         try {
-            mCam = Camera.open(Camera.CameraInfo.CAMERA_FACING_FRONT);
+            mCam = Camera.open(cameraId);
             if (mCam != null) {
                 mCamPreview = new MirrorView(this, mCam);
                 mCamPreview.setCameraDisplayOrientationAndSize();
